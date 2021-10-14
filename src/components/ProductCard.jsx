9a8c4b275c4673bd8d99dac39aa2a2f8e4de0e68
Diana Tineo <diana.tineo1@gmail.com>
2021-10-14 13:57:52 -0400

    my-react-app

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./products.css"
+
+
+
+function ProductCard({brand, type, name, price}){
+  return (
+    <div className="product-card">
+      <small>{brand} / {type}</small>
+      <h3>{name}</h3>
+      <div className="product-price">${price}</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import ProductCard from "./ProductCard";
+
+const productList =[{
+  id:1,
+  name: 'Juan Pollo Tropical',
+  brand: 'Tommy Bahama',
+  type: 'shirt',
+  price: 149.99,
+},
+{
+  id:2,
+  name: 'Boring grey t-shirt',
+  brand: 'Haynes',
+  type: 'shirt',
+  price: 9.99,
+},
+{
+  id:3,
+  name: 'Grey Swoop-neck Blouse',
+  brand: 'Zara',
+  type: 'Blouse',
+  price: 49.99,
+},
+{
+  id:4,
+  name: 'Boca Code Polo',
+  brand: 'America Apparel',
+  type: 'shirt',
+  price: 29.99,
+},]
 
 function Products(){
   return(
+    <>
     <h2>Products</h2>
+    <div className="product-list">
+     {productList.map(item => {
+       return <ProductCard 
+       key={item.id}
+       name={item.name} 
+       brand={item.brand} 
+       price={item.price} 
+       type={item.type} /> 
+     })}
+    </div>
+    </>
   )
 }
 
 
 export default Products
+

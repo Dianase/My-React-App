db83f999ef2f8f4a7a2c8d0b836c358402a54cb9
Diana Tineo <diana.tineo1@gmail.com>
2021-10-14 11:50:58 -0400

    started react-app

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Hero(){
+  return (
+    <section className="hero">
+      <h2>Welcome to</h2>
+      <h1>My Boutique</h1>
+      <button>Shop Now</button>
+    </section>
+  )
+}
+
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import './navbar.css'
+
+function Navbar(){
+  return(
+    <header>
+      <nav>
+        This is our Navbar
+      </nav>
+    </header>
+  )
+}
+
+
+
+export default Navbar
